66826215be56a8437fb1a58719df306cbdd746de
danivarelas <danivarelas@hotmail.com>
2018-05-03 17:55:28 -0600

    se hizo una pantalla deslizante en el perfil del jugador para ver logros y rallies

---
+++ app/src/main/java/com/rallygeologico/PagerAdapter.java
@@ -0,0 +1,57 @@
+package com.rallygeologico;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+class PagerAdapter extends FragmentPagerAdapter {
+
+    private static int NUM_PAGES = 2;
+
+    Context mContext;
+
+    public PagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment;
+        Bundle args;
+        switch (position) {
+            case 0:
+                fragment = new ProfileFragment();
+                args = new Bundle();
+                fragment.setArguments(args);
+                return fragment;
+            case 1:
+                fragment = new ProfileFragment();
+                args = new Bundle();
+                fragment.setArguments(args);
+                return fragment;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_PAGES;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Rallies";
+            case 1:
+                return "Logros";
+        }
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/rallygeologico/ProfileFragment.java
@@ -0,0 +1,18 @@
+package com.rallygeologico;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfileFragment extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
+    }
+}
